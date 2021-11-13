d43e20e3ddd86c904ed4ce500d4281ed301a78aa
Alex <cashish94@list.ru>
2021-11-14 02:04:41 +0400

    add transactions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, } from 'react';
+import './App.css'
+import { runTransaction, doc, getFirestore, onSnapshot } from 'firebase/firestore'
+
 
 function App() {
+  const db = getFirestore()
+  const [value, setValue] = useState(0)
+
+
+  onSnapshot(doc(db, "counters", "counter"), (doc) => {
+    setValue(doc.data().value);
+  });
+
+
+  const increase = async () => {
+    const docRef = doc(db, 'counters', 'counter');
+
+    try {
+      runTransaction(db, async (transaction) => {
+        const document = await transaction.get(docRef)
+        if (!document.exists()) {
+          throw new Error("Document does not exist!")
+        }
+
+        const newValue = document.data().value + 1;
+        transaction.update(docRef, { value: newValue })
+        // console.log("Transaction successfully committed!", newValue)
+      });
+    } catch (e) {
+      console.log("Transaction failed: ", e)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <p>{value}</p>
+      <input type="button" value='Increase' onClick={increase} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app'
+// import { getFirestore } from 'firebase/firestore/lite'
+
+// eslint-disable-next-line
+const app = initializeApp({
+  apiKey: "AIzaSyAOQkszBNubewymxxXLdonq7Y8gR7vmAGc",
+  authDomain: "click-counter-82b99.firebaseapp.com",
+  projectId: "click-counter-82b99",
+  storageBucket: "click-counter-82b99.appspot.com",
+  messagingSenderId: "939751074053",
+  appId: "1:939751074053:web:50c773e7c81ca471b9af6c"
+})
+// const db = getFirestore(app)
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +24,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
